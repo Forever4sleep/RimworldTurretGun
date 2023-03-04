642e504fd79630b6d05fbc3b503f02582bd74754
Forever4sleep <tired.teen1337@gmail.com>
2023-03-04 19:45:39 +0300

    the mod

---
+++ Empty_DarknessStun/Empty_DarknessStun/DamageWorker_LaserExplode.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Empty_DarknessStun
+{
+    public class DamageWorker_LaserExplode : DamageWorker_AddInjury
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            var try_pawn = thing as Pawn;
+            //just in case something goes south, which might be not under my control
+            //i dont want any exceptions to be thrown during the game as it ruins user's experience.
+            try
+            {
+                if ((try_pawn != null && (!try_pawn.Downed || try_pawn.Downed && try_pawn.HealthScale != 0) || thing.BlocksPawn(thing as Pawn) ||(thing as Building) != null))
+                    GenExplosion.DoExplosion(thing.Position, thing.Map, 3f, DamageDefOf.Bomb, dinfo.Instigator, explosionSound: this.def.soundExplosion, damAmount: 20);
+            }
+            catch { }
+
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
